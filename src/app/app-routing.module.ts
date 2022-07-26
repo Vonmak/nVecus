a2efd3f2c221622caad097b3a25048a9160be402
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-26 18:04:02 +0300

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './components/customer/customer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { VendorComponent } from './components/vendor/vendor.component';
+import { AuthGuard } from './guards/auth.guard';
+import { CustomerGuard } from './guards/customer.guard';
+import { VendorGuard } from './guards/vendor.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'signup', component:SignupComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'customer', component:CustomerComponent, canActivate: [AuthGuard,CustomerGuard]},
+  {path: 'vendor', component:VendorComponent, canActivate: [AuthGuard,VendorGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
