fb08747e037ac0fee3db3b2138a97a6e235d4ae2
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-07-26 17:57:45 +0300

    created a signup component.

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(formData: any): void {
+    this.authService.signupUser(formData).subscribe((data) => {
+      console.log(data);
+    })
+  }
+}
